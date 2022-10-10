cea85bf8775f1e6a2930b52e43621eec7c1009ed
Cole McGuire <colejmcguire45@gmail.com>
2022-10-10 14:10:47 -0400

    Establishes connection  to RealTime db

---
+++ Swole Patrol/Swole Patrol/Swole Patrol/Services/ItemRepository.cs
@@ -0,0 +1,14 @@
+﻿using Firebase.Database;
+using Swole_Patrol.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swole_Patrol.Services
+{
+    internal class ItemRepository
+    {
+        FirebaseClient firebaseClient = new FirebaseClient("https://swole-patrol-b7088-default-rtdb.firebaseio.com/");
+    }
+}
